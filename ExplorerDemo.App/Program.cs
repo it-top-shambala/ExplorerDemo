f3186eedc1c85d44f96af89ce1a47243d2cb0e13
Starinin Andrey <starininandrey@gmail.com>
2022-06-12 20:43:41 +0300

    Init

---
+++ ExplorerDemo.App/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExplorerDemo.App
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var disks = DriveInfo.GetDrives();
+            foreach (var disk in disks)
+            {
+                Console.Write($"{disk.Name} {disk.VolumeLabel}, ");
+                Console.Write($"{disk.DriveFormat}, {disk.DriveType}, ");
+                Console.WriteLine($"{disk.TotalSize} - {disk.TotalFreeSpace}");
+
+                var root = disk.RootDirectory;
+                foreach (var directory in root.GetDirectories())
+                {
+                    Console.WriteLine($"{directory.Name}, {directory.CreationTimeUtc:g}");
+                }
+            }
+        }
+    }
+}
